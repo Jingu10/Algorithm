e1e841468dd05db9520ca04d20c8675468761b9a
여진구 <146045578+Jingu10@users.noreply.github.com>
2023-12-08 20:39:43 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,42 +1,45 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/* 9C7이 가능하면 9C2도 가능하다. 두 명을 뽑아서, 전체합 - 두명 합 == 100인지를 검사 */
+
+
 public class Main {
+	static int[] nums = new int[9];
+	static int sumAll = 0;
+	static int[] selected = new int[2]; 
+	static boolean[] visited = new boolean[9]; // 뽑 or 안뽑
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int[] arr = new int[9];
-		int[] real = new int[7];
-		
+		nums = new int[9];
 		for(int i = 0; i < 9; i++) {
-			
-			arr[i] = sc.nextInt();
+			nums[i] = sc.nextInt();
+			sumAll += nums[i];
 		}
-
-		int sum = 0;
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				sum = 0;
-				if (i == j)
-					continue;
-				for (int k = 0; k < 9; k++) {
-					if (k != i && k != j)
-						sum += arr[k];
-				}
-				if (sum == 100) {
-					int index = 0;
-					for (int k = 0; k < 9; k++) {
-						if (k != i && k != j)
-							real[index++] = arr[k];
-					}
-					break;
-				}
-			}
-		}
-
-		Arrays.sort(real);
-		for (int num : real) {
-			System.out.println(num);
-		}
+		Arrays.sort(nums);
+		comb(0);
 	}
 
+	private static void comb(int depth) {
+		if(depth == 2) { // 2명 뽑은 케이스 
+			if(sumAll - selected[0] - selected[1] == 100) {
+				for(int i = 0; i < 9; i++) {
+					if(nums[i] == selected[0] || nums[i] == selected[1]) continue;
+					System.out.println(nums[i]);
+				}
+				System.exit(0);
+			}
+			return;
+		}
+		for(int i = 0; i < 9; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				selected[depth] = nums[i];
+				comb(depth + 1);
+				visited[i] = false;
+			}
+		}
+		
+	}
 }
