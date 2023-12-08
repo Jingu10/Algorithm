32b235173d8d8a942cb16d0ceedcee245bd51d8d
여진구 <146045578+Jingu10@users.noreply.github.com>
2023-12-08 17:35:31 +0900

    [Silver III] Title: N과 M (2), Time: 80 ms, Memory: 11556 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int N, M; // 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열 출력
+	static int[] res;
+	static boolean[] visited; // 뽑았던 수를 또 뽑으면 안되기 때문에 visited 배열 설정
+	
+	public static void main(String[] args) throws IOException {
+		init();
+		perm(0, 1);
+	}
+	
+	
+	private static void perm(int depth, int start) {
+		// M개를 모두 뽑은 경우 출력
+		if(depth == M) {
+			for(int num : res) {
+				System.out.print(num + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		// ex) 2를 뽑고 재귀를 타면 start가 3부터 시작하니까 2 아래는 볼 필요가 없다.
+		for(int i = start; i <= N; i++) {
+			if(!visited[i]) { // 아직안뽑았어
+				visited[i] = true;
+				res[depth] = i;
+				perm(depth + 1, i + 1);
+				visited[i] = false;
+			}
+		}
+	}
+
+
+	static void init() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean[N + 1]; 
+		res = new int[M]; // M개 뽑은 순열
+	}
+}
