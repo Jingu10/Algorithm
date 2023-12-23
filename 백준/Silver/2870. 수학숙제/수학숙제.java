4daa678e695a1a4b8312e59666570ea71eb25e57
여진구 <146045578+Jingu10@users.noreply.github.com>
2023-12-23 23:58:27 +0900

    [Silver IV] Title: 수학숙제, Time: 112 ms, Memory: 16636 KB -BaekjoonHub

--- 백준/Silver/2870. 수학숙제/수학숙제.java
+++ 백준/Silver/2870. 수학숙제/수학숙제.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<BigInteger> list = new ArrayList<>();
+		List<String> list = new ArrayList<>();
 		
 		int N = Integer.parseInt(br.readLine());
 		for(int i = 0; i < N; i++) {
@@ -21,26 +21,61 @@ public class Main {
 					if(numString.equals("")) {
 						continue;
 					} else {
-						list.add(new BigInteger(numString));
+						numString = removeZero(numString);
+						list.add(numString);
 						numString = "";
 					}
 				} else {
 					numString += ch; // 숫자 붙이기
 					if(j == input.length() - 1) {
-						list.add(new BigInteger(numString));
+						numString = removeZero(numString);
+						list.add(numString);
 					}
 				}
 			}
 			
 		}
 		
-		Collections.sort(list, Collections.reverseOrder());
-		for(int i = list.size() - 1; i >= 0; i--) {
-			System.out.println(list.get(i));
+		Collections.sort(list, new Comparator<String>() {
+
+			@Override
+			public int compare(String o1, String o2) {
+				if(o1.length() == o2.length()) {
+					// 사전 순 비교할 때는 compareTo를 활용한다.
+					return o1.compareTo(o2);
+				}
+				
+				return o1.length() - o2.length();
+			}
+			
+		});
+		
+		for(String n : list) {
+			System.out.println(n);
 		}
 	}
+
+	private static String removeZero(String numString) {
+		int notZeroIndex = -1;
+		for(int i = 0; i < numString.length(); i++) {
+			if(numString.charAt(i) != '0') {
+				notZeroIndex = i;
+				break;
+			}
+		}
+		
+		if(notZeroIndex == -1) {
+			// 모든 숫자가 0인 경우
+			return "0";
+		} else {
+			String res = "";
+			for(int i = notZeroIndex; i < numString.length(); i++) {
+				res += numString.charAt(i);
+			}
+			return res;
+		}
+		
+	}
+
 	
 }
-
-// 엄청 큰 숫자 문자열 -> BigInteger를 써보는 방법도 생각은 해 봐야 한다
-// new BigInteger(문자열)
