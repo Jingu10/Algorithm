6f9dfce1d8cfc07b0074c47fc4050e6f34c0c49b
여진구 <146045578+Jingu10@users.noreply.github.com>
2024-01-01 17:52:39 +0900

    [Gold IV] Title: 주난의 난(難), Time: 176 ms, Memory: 21904 KB -BaekjoonHub

---
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -0,0 +1,103 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N, M;
+	static char[][] map;
+	static boolean[][] visited;
+	static Queue<int[]> queue1;
+	static Queue<int[]> queue2;
+	static int jump;
+	static int[] dr = { -1, 0, 1, 0 };
+	static int[] dc = { 0, 1, 0, -1 };
+
+	public static void main(String[] args) throws IOException {
+		init();
+		solve();
+		System.out.println(jump);
+	}
+
+	private static void solve() {
+		while (true) {
+			jump++;
+			if (jump % 2 == 1) {
+				while (!queue1.isEmpty()) {
+					int[] pos = queue1.poll();
+					int row = pos[0];
+					int col = pos[1];
+					for (int d = 0; d < 4; d++) {
+						int nrow = row + dr[d];
+						int ncol = col + dc[d];
+						if (nrow > 0 && ncol > 0 && nrow <= N && ncol <= M && !visited[nrow][ncol]) {
+							visited[nrow][ncol] = true;
+							if (map[nrow][ncol] == '0') {
+								queue1.add(new int[] { nrow, ncol });
+							} else if (map[nrow][ncol] == '1') {
+								queue2.add(new int[] { nrow, ncol });
+								map[nrow][ncol] = 0;
+							} else {
+								return;
+							}
+						}
+					}
+				}
+			} else {
+				while (!queue2.isEmpty()) {
+					int[] pos = queue2.poll();
+					int row = pos[0];
+					int col = pos[1];
+					for (int d = 0; d < 4; d++) {
+						int nrow = row + dr[d];
+						int ncol = col + dc[d];
+						if (nrow > 0 && ncol > 0 && nrow <= N && ncol <= M && !visited[nrow][ncol]) {
+							visited[nrow][ncol] = true;
+							if (map[nrow][ncol] == '0') {
+								queue2.add(new int[] { nrow, ncol });
+							} else if (map[nrow][ncol] == '1') {
+								queue1.add(new int[] { nrow, ncol });
+								map[nrow][ncol] = 0;
+							} else {
+								return;
+							}
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+	static void init() throws IOException {
+		jump = 0;
+		queue1 = new LinkedList<>();
+		queue2 = new LinkedList<>();
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+
+		map = new char[N + 1][M + 1];
+		visited = new boolean[N + 1][M + 1];
+		st = new StringTokenizer(br.readLine());
+		int startRow = Integer.parseInt(st.nextToken());
+		int startCol = Integer.parseInt(st.nextToken());
+		st.nextToken();
+		st.nextToken(); // 필요없는 토큰 소모
+
+		visited[startRow][startCol] = true;
+		queue1.add(new int[] { startRow, startCol });
+
+		for (int i = 1; i <= N; i++) {
+			String line = br.readLine();
+			for (int j = 1; j <= M; j++) {
+				map[i][j] = line.charAt(j - 1);
+			}
+		}
+
+	}
+}
