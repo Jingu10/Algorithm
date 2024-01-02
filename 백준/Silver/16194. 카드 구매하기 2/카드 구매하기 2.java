e6a35b46c80e5863b96389ac2ab669412320dfc1
여진구 <146045578+Jingu10@users.noreply.github.com>
2024-01-02 18:54:54 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 204 ms, Memory: 17416 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] nums = new int[N+1];
+		int[] dp = new int[N + 1];
+		for(int i = 1; i <= N; i++) {
+			nums[i] = sc.nextInt();
+			dp[i] = nums[i];
+		}
+		
+		for(int i = 2; i <= N; i++) {
+			for(int j = 1; j < i; j++) {
+				dp[i] = Math.min(dp[i], dp[i-j] + nums[j]);
+			}
+			
+		}
+		
+		
+		
+		
+		System.out.println(dp[N]);
+	}
+}
